43c958c773121a557ac2cf00afb79b979a8157df
CJarrell <colby.jarrell@icloud.com>
2016-11-11 18:45:46 -0500

    Prettied up the contact form

--- scripts/custom.js
+++ scripts/custom.js
@@ -16,3 +16,16 @@ $(function(){
 //     return false;
 //   });
 // });
+
+// Animated Scrolling
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
